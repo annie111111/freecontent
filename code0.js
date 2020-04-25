04be360f6d457d83359e3f30b6376a2e9024176c
annie111111 <addergaraypacheco@gmail.com>
2020-04-25 03:39:34 -0400

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,35 @@
+gdjs.Nueva_32EscenaCode = {};
+gdjs.Nueva_32EscenaCode.GDNuevoObjetoObjects1= [];
+gdjs.Nueva_32EscenaCode.GDNuevoObjetoObjects2= [];
+
+gdjs.Nueva_32EscenaCode.conditionTrue_0 = {val:false};
+gdjs.Nueva_32EscenaCode.condition0IsTrue_0 = {val:false};
+gdjs.Nueva_32EscenaCode.condition1IsTrue_0 = {val:false};
+
+
+gdjs.Nueva_32EscenaCode.eventsList0x22e208 = function(runtimeScene, context) {
+
+{
+
+
+gdjs.Nueva_32EscenaCode.condition0IsTrue_0.val = false;
+{
+gdjs.Nueva_32EscenaCode.condition0IsTrue_0.val = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+}if (gdjs.Nueva_32EscenaCode.condition0IsTrue_0.val) {
+{gdjs.evtTools.window.openURL("https://jl0qd.app.link/rvzFsgHnvY");
+}}
+
+}
+
+
+}; //End of gdjs.Nueva_32EscenaCode.eventsList0x22e208
+
+
+gdjs.Nueva_32EscenaCode.func = function(runtimeScene, context) {
+context.startNewFrame();
+gdjs.Nueva_32EscenaCode.GDNuevoObjetoObjects1.length = 0;
+gdjs.Nueva_32EscenaCode.GDNuevoObjetoObjects2.length = 0;
+
+gdjs.Nueva_32EscenaCode.eventsList0x22e208(runtimeScene, context);return;
+}
+gdjs['Nueva_32EscenaCode']= gdjs.Nueva_32EscenaCode;
